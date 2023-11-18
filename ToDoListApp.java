49158cd0161ed9341b1de3af3fedfe0f08e2bd9e
Sifat Ahmed <151382725+SifatAhmed28@users.noreply.github.com>
2023-11-18 21:18:25 +0600

    Update ToDoListApp.java

--- ToDoListApp.java
+++ ToDoListApp.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package com.mycompany.todolistapp;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
